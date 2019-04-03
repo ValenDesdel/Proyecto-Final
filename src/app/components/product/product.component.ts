a89294ae00f37b6e406564fce28aee54d2ff45a0
ValenDesdel <vdestefano@correo.unimet.edu.ve>
2019-04-03 12:08:45 -0400

    Listo detalle de productos

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { DataApiService } from '../../services/data-api.service';
+import { ProductInterface } from '../../models/product';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-product',
@@ -7,9 +10,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductComponent implements OnInit {
 
-  constructor() { }
+  constructor(private dataApi: DataApiService, private route: ActivatedRoute) {}
+  public product: ProductInterface = {};
 
   ngOnInit() {
+    const idProduct = this.route.snapshot.params["id"];
+    this.dataApi.getOneProduct(idProduct).subscribe( product =>{
+      console.log("Detallesproduct", product);
+    });
   }
 
 }
